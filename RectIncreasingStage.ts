7e828aba4150e85642cb07563db7cbd1a7d7ddeb
Anwesh43 <anweshthecool0@gmail.com>
2018-07-14 23:39:31 +0530

    adding Stage clas

---
+++ RectIncreasingStage.ts
@@ -0,0 +1,28 @@
+const w : number = window.innerWidth, h = window.innerHeight, nodes = 5
+class RectIncreasingStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+
+    context : CanvasRenderingContext2D
+
+    constructor() {
+        this.initCanvas()
+    }
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+}
