a79c3ce4053f9f04507bdf8b64a56790307b0f20
Anwesh43 <anweshthecool0@gmail.com>
2018-07-15 00:04:25 +0530

    adding canvas to body

--- RectIncreasingStage.ts
+++ RectIncreasingStage.ts
@@ -17,6 +17,7 @@ class RectIncreasingStage {
         this.canvas.width = w
         this.canvas.height = h
         this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
     }
 
     render() {
